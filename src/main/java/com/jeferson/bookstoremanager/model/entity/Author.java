b13cd999defceccb279ff4dc3b10cf512bf1e5fb
Jeferson Prestes <jeferson_prestes@outlook.com>
2022-06-17 22:39:27 -0300

    Developed project entities

---
+++ src/main/java/com/jeferson/bookstoremanager/model/entity/Author.java
@@ -0,0 +1,31 @@
+package com.jeferson.bookstoremanager.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false)
+	private Integer age;
+	
+}
